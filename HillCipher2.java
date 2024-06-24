cdbc07f84b461e4f777bfdd6ac9d5de16f611000
MGIT-GitHub <visibleproton@gmail.com>
2024-06-24 10:49:13 +0530

    Create HillCipher2.java

---
+++ HillCipher2.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+class HillCipher2 {
+    // Function to get the key matrix from the key string
+    static void getKeyMatrix(String key, int keyMatrix[][]) {
+        int k = 0;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                keyMatrix[i][j] = (key.charAt(k)) % 65;
+                k++;
+            }
+        }
+    }
+
+    // Function to perform matrix multiplication and find the cipher matrix
+    static void encrypt(int cipherMatrix[][], int keyMatrix[][], int messageVector[][]) {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 1; j++) {
+                cipherMatrix[i][j] = 0;
+                for (int k = 0; k < 2; k++) {
+                    cipherMatrix[i][j] += keyMatrix[i][k] * messageVector[k][j];
+                }
+                cipherMatrix[i][j] = cipherMatrix[i][j] % 26;
+            }
+        }
+    }
+
+    // Function to encrypt the message
+    static void HC(String message, String key) {
+        int[][] keyMatrix = new int[2][2];
+        getKeyMatrix(key, keyMatrix);
+
+        int[][] messageVector = new int[2][1];
+        for (int i = 0; i < 2; i++) {
+            messageVector[i][0] = (message.charAt(i)) % 65;
+        }
+
+        int[][] cipherMatrix = new int[2][1];
+        encrypt(cipherMatrix, keyMatrix, messageVector);
+
+        StringBuilder ciphertext = new StringBuilder();
+        for (int i = 0; i < 2; i++) {
+            ciphertext.append((char) (cipherMatrix[i][0] + 65));
+        }
+
+        System.out.println("Cipher text: " + ciphertext);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the plain text (2 letters): ");
+        String message = sc.nextLine().toUpperCase();
+        System.out.println("Enter the key (4 letters): ");
+        String key = sc.nextLine().toUpperCase();
+
+        HC(message, key);
+    }
+}
